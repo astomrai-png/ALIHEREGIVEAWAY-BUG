45c4f844b5f7659071b52bf17f2cb27c6eb185ae
astomrai-png <2.70137229e+08+astomrai-png@users.noreply.github.com>
2026-08-26 07:30:42 +0000

    Initial upload of ALIHEREGIVEAWAY BUG

---
+++ config.js
@@ -0,0 +1,16 @@
+
+module.exports = {
+    mainToken: process.env.TELEGRAM_BOT_TOKEN || '', // Set in environment
+    
+    S7: 'ROWDY HERE BUG', // Owner, Name
+    
+    adminId: process.env.TELEGRAM_ADMIN_ID || '', // Set in environment
+    
+    channel: 'https://t.me/astomrai123', // TG channel link
+    
+    group: 'https://t.me/rowdyboy123456', // TG group link
+    
+    bot: 'ROWDY HERE BUG', // Bot name 
+    
+    logo: './SY/Loves.jpg' // Bot logo path or URL
+};
